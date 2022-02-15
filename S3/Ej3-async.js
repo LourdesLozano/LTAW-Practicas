6eb26f713a6bb5aef861ab14f032c5b2dfd01f34
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-02-15 14:22:42 +0100

    eje3 coment

--- S3/Ej3-async.js
+++ S3/Ej3-async.js
@@ -28,6 +28,7 @@ console.log("MENSAJE E");
 server.listen(PUERTO);
 console.log("MENSAJE F");
 
+// SITUACION 1 --------------------------------------------------
 // --- Prrimero se arranca el servidor y sale el menaje E
 //      servidor modo escucha y sale mensaje F
 
@@ -38,3 +39,27 @@ console.log("MENSAJE F");
 // --- Termina funcion de retrollamada
 //      llega evento end 
 //      sale mensaje C
+
+// terminal: node ...
+// otro temrinal: curl "127.0.0.1:8080"
+
+
+// SITUACION 2 --------------------------------------------------
+// --- Prrimero se arranca el servidor y sale el menaje E
+//      servidor modo escucha y sale mensaje F
+
+// --- recibimos mensaje de solucitud y se va  ala funcion de retrollamada
+//      sale mensaje A
+//      sale mensaje D
+//      sale mensaje A
+
+//      llega evento "data"
+//      sale mensaje B
+//      sale mensaje A
+
+//      Llega el evento "end"
+//      sale mensaje A
+//      sale mensaje C
+
+// terminal: node ...
+// otro temrinal: curl -d "holaaaa..." "127.0.0.1:8080"
