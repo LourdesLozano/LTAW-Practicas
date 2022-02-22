81460720a1d9a306b2009c191e276a0059ec2a88
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-02-22 13:08:11 +0100

    coment especific

---
+++ P1/tienda.js
@@ -0,0 +1,5 @@
+// servidor debe esccuhar en puerto 9090
+// si hay recurso no disponible --> genera página html de error
+
+// --- FRONT END
+// página principal con 3 productos diferentes con imagem y enlace propia
