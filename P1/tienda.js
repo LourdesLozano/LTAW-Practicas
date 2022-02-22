ac03babb9bc49e34ba97e705a31febc664bf8178
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-02-22 13:30:13 +0100

    code

--- P1/tienda.js
+++ P1/tienda.js
@@ -7,3 +7,91 @@
 // 1. para lanzar servidor: node tienda.js
 // 2. abrir navegador: http://localhost:9000
 
+// -- el servidor de mi tienda
+
+
+// fichero html 
+// ficheros con img
+// ficheros css
+// devolver fichero pedido
+
+
+// -- PRACTICA 1: TIENDA ON-LINE CON NODE.JS
+//-- Importamos los modulos http, fs y url
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+//-- Definir el puerto a utilizar
+const port = 9090;
+
+//-- Mensaje de arranque
+console.log("Arrancando servidor...");
+
+//-- Crear el sevidor
+const server = http.createServer(function (req, res) {
+    
+    //-- Url que pide el cliente
+    const myUrl = new URL(req.url, 'http://' + req.headers['host']);
+    console.log("\nRecurso recibido: " + myUrl.pathname);
+
+    //-- Escribir en consola la ruta de nuestro recurso
+    console.log("---> Peticion Recibida: " + myUrl);
+
+    var mine = {
+        '/'    : 'text/html',
+        'html' : 'text/html',
+        'css'  : 'text/css',
+        'jfif' : 'image/jfif',
+        'png'  : 'image/png',
+        'gif'  : 'image/gif',
+      
+    };
+    
+    let filename = ""
+
+    //-- Obtenemos el fichero correspondiente.
+    if(myUrl.pathname == '/'){
+        filename += "./tienda.html"; //-- Página principal de la tienda
+
+    }else{
+        filename += "." + myUrl.pathname;
+    }
+
+    console.log("Filename:", filename);
+
+    // -- Buscamos el "." final para poder indicar que tipo mine es
+    let hastaPunto = myUrl.pathname.lastIndexOf(".");
+    let type = myUrl.pathname.slice(hastaPunto + 1);
+    console.log("Tipo de mine:", mine[type])
+
+    //-- Respuesta por defecto
+    let code = 200;
+    let message = "OK";
+
+    //-- Leemos fichero
+    fs.readFile(filename, (err, data) => {
+
+        //si hay error
+        if (err) {
+            // fichero de error
+
+        //si no hay error
+        }else{
+            res.statusCode = code; 
+            res.statusMessage = message;
+            res.writeHead(code, {'Content-Type': mine[type]});
+            res.write(data);
+            res.end();
+        }
+
+    });
+
+});
+
+//-- Activar el servidor
+server.listen(port);
+
+//-- Mensaje de inicio
+console.log("Server esta activo. \nEscuchando en puerto: " + port);
+
