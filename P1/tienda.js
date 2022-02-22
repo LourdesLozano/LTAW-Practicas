4dc628785c744443fde9a066cdefdd533bc65057
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-02-22 13:13:07 +0100

    coment

--- P1/tienda.js
+++ P1/tienda.js
@@ -3,3 +3,7 @@
 
 // --- FRONT END
 // página principal con 3 productos diferentes con imagem y enlace propia
+
+// 1. para lanzar servidor: node tienda.js
+// 2. abrir navegador: http://localhost:9000
+
