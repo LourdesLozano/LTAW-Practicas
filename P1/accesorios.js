1e18ffbb426228596313a995b31cca8e850a8fdb
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-03-02 19:39:11 +0100

    estilo

--- P1/accesorios.js
+++ P1/accesorios.js
@@ -4,13 +4,24 @@ const btn_collares = document.getElementById("btn_collares");
 const btn_abrigos = document.getElementById("btn_collares");
 const btn_lazos = document.getElementById("btn_collares");
 
+// collares
+const collar1 = document.getElementById("collar1");
+const collar2 = document.getElementById("collar2");
+const collar3 = document.getElementById("collar3");
+
 // Que no aaprezca ningun elemento
-//document.getElementById('productos').style.display = 'none';
+document.getElementById('productos').style.display = 'none';
+document.getElementById('texto').style.display = 'none';
 
 // COLLARES
 btn_collares.onclick = () => {
     document.getElementById('productos').style.display = 'block';
 }
+
+collar1.onclick = () => {
+    document.getElementById('texto').style.display = 'block';
+}
+
 
 // ABRIGOS
 btn_abrigos.onclick = () => {
