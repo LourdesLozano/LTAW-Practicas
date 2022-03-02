eb8a9e1b8b67335e803d540965542c1e801a3718
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-03-02 18:08:43 +0100

    cambios

---
+++ P1/accesorios.js
@@ -0,0 +1,7 @@
+
+// BOTONES
+const btn_collares = document.getElementById("btn_collares");
+const btn_abrigos = document.getElementById("btn_collares");
+const btn_lazos = document.getElementById("btn_collares");
+
+// Que no aaprezca ningun elemento
