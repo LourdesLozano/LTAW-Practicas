3b3a272ecc5710de3bb7c19feb7ae60d6536223f
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-03-02 18:56:48 +0100

    activando unos botones

--- P1/accesorios.js
+++ P1/accesorios.js
@@ -6,3 +6,18 @@ const btn_lazos = document.getElementById("btn_collares");
 
 // Que no aaprezca ningun elemento
 document.getElementById('productos').style.display = 'none';
+
+// COLLARES
+btn_collares.onclick = () => {
+    document.getElementById('productos').style.display = 'block';
+}
+
+// ABRIGOS
+btn_abrigos.onclick = () => {
+    document.getElementById('productos').style.display = 'block';
+}
+
+//LAZOS
+btn_lazos.onclick = () => {
+    document.getElementById('productos').style.display = 'block';
+}
