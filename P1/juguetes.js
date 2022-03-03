d0b4024cb9944d1924a1b0b2501d3888dd3152a4
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-03-03 17:07:02 +0100

    escondemos texto

--- P1/juguetes.js
+++ P1/juguetes.js
@@ -3,8 +3,6 @@ const juguete1 = document.getElementById("juguete1");
 const juguete2 = document.getElementById("juguete2");
 
 // Que no aaprezca ningun elemento--------------------------------------
-document.getElementById('productos6').style.display = 'none'; // juguetes
-
 document.getElementById('texto14').style.display = 'none';
 document.getElementById('texto15').style.display = 'none';
 document.getElementById('texto16').style.display = 'none';
