1d94e9f86b0b904fad3e15c345c988adc2ebb1c3
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-03-03 17:06:37 +0100

    primeros cambios js botones jueguetes

---
+++ P1/juguetes.js
@@ -0,0 +1,11 @@
+// BOTONES------------------------------------------------------
+const juguete1 = document.getElementById("juguete1");
+const juguete2 = document.getElementById("juguete2");
+
+// Que no aaprezca ningun elemento--------------------------------------
+document.getElementById('productos6').style.display = 'none'; // juguetes
+
+document.getElementById('texto14').style.display = 'none';
+document.getElementById('texto15').style.display = 'none';
+document.getElementById('texto16').style.display = 'none';
+document.getElementById('texto17').style.display = 'none';
