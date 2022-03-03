f2054af616d9efd1c043da30d7db0e0d2596f924
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-03-03 20:15:55 +0100

    cambios e

--- P1/juguetes.js
+++ P1/juguetes.js
@@ -1,28 +1,16 @@
 // BOTONES------------------------------------------------------
-const btn_juguetes = document.getElementById("btn_juguetes")
-
 const juguete1 = document.getElementById("juguete1");
 const juguete2 = document.getElementById("juguete2");
 const juguete3 = document.getElementById("juguete3");
 const juguete4 = document.getElementById("juguete4");
 
 // Que no aaprezca ningun elemento--------------------------------------
-document.getElementById('productos6').style.display = 'none';
-
 document.getElementById('texto14').style.display = 'none';
 document.getElementById('texto15').style.display = 'none';
 document.getElementById('texto16').style.display = 'none';
 document.getElementById('texto17').style.display = 'none';
 
 // JUGUETES--------------------------------------------------------------
-btn_juguetes.onclick = () => {
-    document.getElementById('productos6').style.display = 'block';
-    document.getElementById('texto14').style.display = 'none';
-    document.getElementById('texto15').style.display = 'none';
-    document.getElementById('texto16').style.display = 'none';
-    document.getElementById('texto17').style.display = 'none';
-}
-
 juguete1.onclick = () => {
     document.getElementById('texto14').style.display = 'block';
 }
