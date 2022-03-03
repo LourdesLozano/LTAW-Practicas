efc38ab870bf5509ff0b1f34ef0ada3feebaabc4
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-03-03 18:26:14 +0100

    cambios

--- P1/juguetes.js
+++ P1/juguetes.js
@@ -13,7 +13,7 @@ document.getElementById('texto16').style.display = 'none';
 document.getElementById('texto17').style.display = 'none';
 
 // JUGUETES--------------------------------------------------------------
-juguete1.onclick = () => {
+btn_juguetes.onclick = () => {
     document.getElementById('productos6').style.display = 'block';
     document.getElementById('texto14').style.display = 'none';
     document.getElementById('texto15').style.display = 'none';
