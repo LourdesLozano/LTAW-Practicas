752af21e43299305310eb987d3ae1ee9b70d07be
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-03-03 17:11:00 +0100

    js botones

--- P1/juguetes.js
+++ P1/juguetes.js
@@ -7,3 +7,17 @@ document.getElementById('texto14').style.display = 'none';
 document.getElementById('texto15').style.display = 'none';
 document.getElementById('texto16').style.display = 'none';
 document.getElementById('texto17').style.display = 'none';
+
+// JUGUETES--------------------------------------------------------------
+juguete1.onclick = () => {
+    document.getElementById('texto14').style.display = 'block';
+}
+juguete2.onclick = () => {
+    document.getElementById('texto15').style.display = 'block';
+}
+juguete3.onclick = () => {
+    document.getElementById('texto16').style.display = 'block';
+}
+juguete4.onclick = () => {
+    document.getElementById('texto17').style.display = 'block';
+}
