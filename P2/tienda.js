87af17f5018c3eb652efdbb31ce8f78fb4520a00
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-05-16 23:31:35 +0200

    modificando

--- P2/tienda.js
+++ P2/tienda.js
@@ -19,7 +19,6 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const { monitorEventLoopDelay } = require('perf_hooks');
 
 //-- Definir el puerto a utilizar
 const port = 9090;
