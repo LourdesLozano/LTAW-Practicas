b87cf1d32f28895274e246beb95f1b6f4fc5383b
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-05-20 11:44:21 +0200

    cliente

--- P3/chat-cliente.js
+++ P3/chat-cliente.js
@@ -1,25 +1,16 @@
-//-- Cuadrados chat
+
+
 const display = document.getElementById('display');
 const mensaje = document.getElementById('mensaje');
 const enviar = document.getElementById('enviar');
 
 
-//-- Conexión con el server
 const socket = io();
-
-let escribiendo = false;
 
 //-- Mensaje recibido
 socket.on('message', (msg) =>{
     display.innerHTML += '<p>' + ' > ' + msg + '</p>';
 });
-
-mensaje.oninput = () => {
-    if (!escribiendo){
-        socket.send('Otra maruja está escribiendo información...');
-        escribiendo = true; 
-    }
-}
 
 //-- Envío de mensaje al pulsar enviar
 enviar.onclick = () => {
