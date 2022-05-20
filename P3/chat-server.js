95f424d4ef64ecb6c1e37aec9bb2a02329f2a977
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-05-20 11:49:37 +0200

    bienvenida chat

---
+++ P3/chat-server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const express = require('express');
+const socket = require('socket.io');
+const colors = require('colors');
+
+
+
+const PUERTO = 9090;
+
+//-- Constantes chat
+const welcome = '¡BIENVENIDO AL CHAT!';
+const usuario = 'Alguien nuevo quiere cotillear';
+
+
+//-- Directorio público 
+app.use(express.static('public'));
+
+//-- Websockets
+io.on('connection', (socket) => {
+  
+    //-- Nuevo usuario  
+    console.log('-- ¡ALERTA! NUEVA MARUJA --'.pink);
+    connect_count += 1;
+    socket.send(welcome);
+    socket.broadcast.emit('message', usuario); 
+
+  
+});
+
+//-- Lanzar el server
+server.listen(PUERTO);
+console.log('Escuchando en puerto: ' + PUERTO);
