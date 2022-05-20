6040f1c150f66826c7799e4cd92e0dc7a6e34a7c
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-05-20 16:17:10 +0200

    añadimos comando help

--- P3/chat-server.js
+++ P3/chat-server.js
@@ -5,23 +5,24 @@ const colors = require('colors');
 
 const PUERTO = 9090;
 
-
-const welcome = 'Biwnvenidooooo';
+const commandos = 'Comandos: /help, /list, /hello y /date';
+const welcome = 'Bienvenidooo';
 const bye = '¡Adiós!';
-const usuario = 'Usuario nuevo';
-
+const hello = 'Informacion disponible';
+const usuario = 'Nuevo usuario';
 
 //-- Server
 const app = express();
 const server = http.Server(app);
 const io = socket(server);
 
+let connect_count = 0;
 
 //-- Entrada web
 app.get('/', (req, res) => {
-    console.log("eeeee");
     let path = __dirname + '/chat.html';
     res.sendFile(path);
+    console.log("Acceso a " + path);
 });
 
 app.use('/', express.static(__dirname +'/'));
@@ -31,7 +32,7 @@ app.use('/', express.static(__dirname +'/'));
 io.on('connection', (socket) => {
   
     //-- Nuevo usuario  
-    console.log('-- Nuevo user--'.pink);
+    console.log('-- Nuevo usuario --'.pink);
     connect_count += 1;
     socket.send(welcome);
     socket.broadcast.emit('message', usuario);
@@ -47,9 +48,20 @@ io.on('connection', (socket) => {
   socket.on("message", (msg)=> {
     console.log('Mensaje: ' + msg.yellow);
 
-    
-    
-   
+    const date = new Date(Date.now());
+
+    if (msg.startsWith('/')) {
+      console.log('Comandos'.blue);
+      switch(msg){
+        case '/help':
+          console.log('Lista de comandos'.blue);
+          socket.send(commandos);
+          break;
+        
+      }
+    } else {
+      io.send(msg);
+    }; 
   });
 });
 
