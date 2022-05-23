e889f00e572e2786b89869f8d2846b62c7161eb3
LourdesLozano <l.lozano.2017@alumnos.urjc.es>
2022-05-23 16:52:49 +0200

    declaramos variables del html

---
+++ P4/index.js
@@ -0,0 +1,12 @@
+const electron = require('electron');
+
+console.log("Hola desde el proceso de la web...");
+
+//-- Obtener elementos de la interfaz
+const btn_test = document.getElementById("btn_test");
+const display = document.getElementById("display");
+const info1 = document.getElementById("info1");
+const info2 = document.getElementById("info2");
+const info3 = document.getElementById("info3");
+const info4 = document.getElementById("info4");
+const connect_count = document.getElementById("usuarios_conect");
